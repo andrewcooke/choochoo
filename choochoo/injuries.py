1ed87387dbe2448eb27272edae1a6f381017919a
andrew cooke <andrew@acooke.org>
2018-07-15 20:20:26 -0400

    add button for injuries

--- choochoo/injuries.py
+++ choochoo/injuries.py
@@ -1,5 +1,5 @@
 
-from urwid import WEIGHT, Edit, Pile, Columns, connect_signal
+from urwid import WEIGHT, Edit, Pile, Columns, connect_signal, Padding, SolidFill, Text
 
 from .log import make_log
 from .squeal.binders import Binder
@@ -15,15 +15,19 @@ from .widgets import App
 
 class InjuryWidget(FocusWrap):
 
-    def __init__(self, log, tabs, bar):
+    def __init__(self, log, tabs, bar, app=None):
 
         factory = Factory(tabs=tabs, bar=bar)
+        self.__app = app
         self.title = factory(Edit(caption='Title: '))
         self.start = factory(Nullable('Open', lambda date: TextDate(log, bar=bar), bar=bar))
         self.finish = factory(Nullable('Open', lambda date: TextDate(log, bar=bar), bar=bar))
         self.sort = factory(Edit(caption='Sort: '))
-        self.raw_reset = SquareButton('Reset')
-        self.reset = factory(self.raw_reset, message='reset from database')
+        if app:
+            self.__raw_add = SquareButton('Add')
+            add = factory(self.__raw_add, message='add to database')
+        self.__raw_reset = SquareButton('Reset')
+        reset = factory(self.__raw_reset, message='reset from database')
         self.description = factory(Edit(caption='Description: ', multiline=True))
         super().__init__(
             Pile([self.title,
@@ -33,16 +37,23 @@ class InjuryWidget(FocusWrap):
                            ColSpace(),
                            (WEIGHT, 3, self.sort),
                            ColSpace(),
-                           (9, self.reset),
+                           (7, add if app else Text('')),  # to keep spacing regular
+                           (9, reset)
                            ]),
                   self.description,
                   ]))
 
     def connect(self, binder):
-        connect_signal(self.raw_reset, 'click', lambda widget: binder.refresh())
+        connect_signal(self.__raw_reset, 'click', lambda widget: binder.refresh())
+        if self.__app:
+            connect_signal(self.__raw_add, 'click', self.__app.rebuild)
 
 
 class Injuries(DynamicContent):
+
+    def __init__(self, log, session, bar, app):
+        self.__app = app
+        super().__init__(log, session, bar)
 
     def _make(self):
         tabs = TabList()
@@ -52,7 +63,7 @@ class Injuries(DynamicContent):
             widget.connect(Binder(self._log, self._session, widget, Injury, defaults={'id': injury.id}))
             body.append(widget)
         # and a blank entry to be added if necessary
-        widget = InjuryWidget(self._log, tabs, self._bar)
+        widget = InjuryWidget(self._log, tabs, self._bar, app=self.__app)
         widget.connect(Binder(self._log, self._session, widget, Injury))
         body.append(widget)
         return DividedPile(body), tabs
@@ -63,7 +74,7 @@ class InjuryApp(App):
     def __init__(self, log, session, bar):
         self.__session = session
         tabs = TabList()
-        self.injuries = tabs.append(Injuries(log, session, bar))
+        self.injuries = tabs.append(Injuries(log, session, bar, self))
         super().__init__(log, 'Diary', bar, self.injuries, tabs, session)
 
     def rebuild(self, unused_widget, unused_value):
