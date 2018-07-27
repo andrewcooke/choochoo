b9e826c133861a23e93262377bdc36628071e0f1
andrew cooke <andrew@acooke.org>
2018-07-27 08:09:37 -0400

    profile parsing complete

--- choochoo/fit/profile.py
+++ choochoo/fit/profile.py
@@ -14,15 +14,21 @@ def read_profile(log, path):
 
 class ReadOnlyDict(Mapping):
 
-    def __init__(self, log):
+    def __init__(self, log, error_msg):
         self._log = log
+        self.__error_msg = error_msg
         self.__data = {}
 
     def _add(self, name, value):
         self.__data[name] = value
 
     def __getitem__(self, item):
-        return self.__data[item]
+        try:
+            return self.__data[item]
+        except KeyError:
+            msg = self.__error_msg % item
+            self._log.error(msg)
+            raise KeyError(msg)
 
     def __iter__(self):
         return iter(self.__data)
@@ -33,17 +39,17 @@ class ReadOnlyDict(Mapping):
 
 class NamedDict(ReadOnlyDict):
 
-    def __init__(self, log, name):
-        super().__init__(log)
+    def __init__(self, log, error_msg, name):
+        super().__init__(log, error_msg)
         self.name = name
 
 
 class Type(NamedDict):
 
     def __init__(self, log, name, base, rows):
-        super().__init__(log, name)
+        super().__init__(log, 'No value for "%s"', name)
         self.base_type = base
-        self.__comments = {}
+        self.__comments = ReadOnlyDict(self._log, 'No comment for "%s"')
         for row in rows:
             if row[0] or not row[2]:
                 rows.prepend(row)
@@ -58,7 +64,7 @@ class Type(NamedDict):
             try:
                 value = int(row[3], 0)
             except TypeError:
-                self._log.error('Cannot parse value "%s" (type %s)' % (row[3], type(row[3])))
+                self.__log.error('Cannot parse value "%s" (type %s)' % (row[3], type(row[3])))
                 raise
         self._add(name, value)
         self._add(value, name)
@@ -67,7 +73,7 @@ class Type(NamedDict):
 class Types(ReadOnlyDict):
 
     def __init__(self, log, sheet):
-        super().__init__(log)
+        super().__init__(log, 'No type for "%s"')
         self.base_types = set()
         rows = peekable([cell.value for cell in row] for row in sheet.iter_rows())
         for row in rows:
@@ -79,53 +85,93 @@ class Types(ReadOnlyDict):
                 self._add(row[0], Type(self._log, row[0], row[1], rows))
 
 
-class MessageData:
+class SimpleMessageField:
 
-    def __init__(self, log, name, number, row, rows, types):
+    def __init__(self, log, name, number, row, types):
         self._log = log
         self.name = name
         self.number = number
-        type = row[3]
+        self.type_name = row[3]
         try:
-            self.type = types[type]
+            self.type = types[self.type_name]
             self.is_base_type = False
         except KeyError:
             if type not in types.base_types:
                 self._log.warn('Additional base type: %s' % type)
                 types.base_types.add(type)
             self.is_base_type = True
-        # drop dynamic for now
+
+    def parse(self, value):
+        if self.is_base_type:
+            if self.type_name == 'string':
+                return value
+            elif 'int' in self.type_name:
+                return int(value, 0)
+            else:
+                return float(value)
+        else:
+            return self.type[value]
+
+
+class MessageField(SimpleMessageField):
+
+    def __init__(self, log, name, number, row, rows, types):
+        super().__init__(log, name, number, row, types)
+        self.is_dynamic = False
+        self.__dynamic_store = []
+        self.dynamic = ReadOnlyDict(self._log, 'No dynamic match for "%s"')
+        self.dynamic_fields = set()
         try:
             peek = rows.peek()
-            while peek[2] and not peek[1]:
-                self._log.warn('Skipping %s' % next(rows))
+            while peek[2] and peek[1] is None:
+                row = next(rows)
+                for name, value in zip(row[11].split(','), row[12].split(',')):
+                    self.__store_dynamic(name.strip(), value.strip(), row)
                 peek = rows.peek()
         except StopIteration:
             return
+
+    def __store_dynamic(self, dynamic_name, dynamic_value, row):
+        self.is_dynamic = True
+        self.__dynamic_store.append((dynamic_name, dynamic_value, row))
+
+    def _complete_dynamic(self, message, types):
+        for dynamic_name, dynamic_value, row in self.__dynamic_store:
+            field = message[dynamic_name]
+            value = field.parse(dynamic_value)
+            self.dynamic_fields.add(field)
+            self.dynamic._add((dynamic_name, value), SimpleMessageField(self._log, row[2], None, row, types))
 
 
 class Message(NamedDict):
 
     def __init__(self, log, name, rows, types):
-        super().__init__(log, name)
+        super().__init__(log, 'No field for "%s"', name)
         for row in rows:
             if not row[2]:
                 rows.prepend(row)
-                return
+                break
             self.__parse(row, rows, types)
+        self.__complete_dynamic(types)
 
     def __parse(self, row, rows, types):
         number = int(row[1])
         name = row[2]
-        data = MessageData(self._log, name, number, row, rows, types)
+        data = MessageField(self._log, name, number, row, rows, types)
         self._add(name, data)
         self._add(number, data)
+
+    def __complete_dynamic(self, types):
+        # these may be forward references
+        for data in self.values():
+            if data.is_dynamic:
+                data._complete_dynamic(self, types)
 
 
 class Messages(ReadOnlyDict):
 
     def __init__(self, log, sheet, types):
-        super().__init__(log)
+        super().__init__(log, 'No message for "%s"')
         self.__messages = {}
         rows = peekable([cell.value for cell in row] for row in sheet.iter_rows())
         for row in rows:
--- tests/test_fit.py
+++ tests/test_fit.py
@@ -6,10 +6,29 @@ from choochoo.fit.profile import read_profile
 
 
 def test_profile():
+
     basicConfig(stream=stdout, level=DEBUG)
     log = getLogger()
     types, messages = read_profile(log, 'data/Profile.xlsx')
+
     cen = types['carry_exercise_name']
     print(cen.name, cen.base_type)
     for key, value in cen.items():
         print(key, value)
+
+    session = messages['session']
+    data = session['total_cycles']
+    assert data.is_dynamic
+    for field in data.dynamic_fields:
+        assert field.name == 'sport'
+    keys = ','.join('%s:%s' % (name, value) for name, value in sorted(data.dynamic.keys()))
+    assert keys == 'sport:1,sport:11', keys
+
+    workout_step = messages['workout_step']
+    data = workout_step[4]
+    assert data.name == 'target_value', data.name
+    assert data.is_dynamic
+    fields = ','.join(sorted(field.name for field in data.dynamic_fields))
+    assert fields == 'duration_type,target_type', fields
+    keys = ','.join('%s:%s' % (name, value) for name, value in sorted(data.dynamic.keys()))
+    assert keys == 'duration_type:6,duration_type:7,duration_type:8,duration_type:9,duration_type:10,duration_type:11,duration_type:12,duration_type:13,target_type:0,target_type:1,target_type:3,target_type:4,target_type:11', keys
