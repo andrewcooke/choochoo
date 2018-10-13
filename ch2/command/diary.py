eb8b91309269ea1cc0f739923e0be7c3ff389b1b
andrew cooke <andrew@acooke.org>
2018-10-13 07:18:29 -0300

    move app

--- ch2/command/diary.py
+++ ch2/command/diary.py
@@ -1,20 +1,20 @@
 
 import datetime as dt
-from abc import abstractmethod
 
 from sqlalchemy import or_
-from urwid import MainLoop, ExitMainLoop, Columns, Pile, Frame, Filler, Text, Divider, WEIGHT
+from urwid import MainLoop, Columns, Pile, Frame, Filler, Text, Divider, WEIGHT
 
-from ch2.uweird.tui.factory import Factory
 from .args import DATE
-from ..lib.date import to_date, YEAR, MONTH, WEEK, DAY, add_duration
+from ..lib.date import to_date
 from ..lib.io import tui
 from ..lib.utils import PALETTE
+from ..lib.widgets import DateSwitcher
 from ..squeal.database import Database
 from ..squeal.tables.topic import Topic, TopicJournal
 from ..uweird.fields import PAGE_WIDTH
 from ..uweird.tui.decorators import Border, Indent
-from ..uweird.tui.tabs import TabNode, TabList
+from ..uweird.tui.factory import Factory
+from ..uweird.tui.tabs import TabList
 from ..uweird.tui.widgets import DividedPile
 
 
@@ -41,85 +41,6 @@ To exit, alt-q (or, without saving, alt-x).
             date = dt.date.today() - dt.timedelta(days=int(date))
     db = Database(args, log)
     MainLoop(Diary(log, db, date), palette=PALETTE).run()
-
-
-class App(TabNode):
-    '''
-    An urwid mainlopp, database session and tabs.
-    '''
-
-    def __init__(self, log, db):
-        self._log = log
-        self.__db = db
-        self.__session = None
-        super().__init__(log, *self._build(self.__new_session()))
-
-    def __new_session(self):
-        self.save()
-        self.__session = self.__db.session()
-        return self.__session
-
-    def keypress(self, size, key):
-        if key == 'meta q':
-            self.save()
-            raise ExitMainLoop()
-        elif key == 'meta x':
-            raise ExitMainLoop()
-        elif key == 'meta s':
-            self.save()
-        else:
-            return super().keypress(size, key)
-
-    def save(self):
-        if self.__session:
-            self._log.debug('Flushing and committing')
-            self.__session.flush()
-            self.__session.commit()
-            self.__session = None
-
-    @abstractmethod
-    def _build(self, session):
-        pass
-
-    def rebuild(self):
-        widget, tabs = self._build(self.__new_session())
-        self._w = widget
-        self.replace(tabs)
-
-
-class DateSwitcher(App):
-    '''
-    Extend App with shortcuts for changing date and rebuilding.
-    '''
-
-    def __init__(self, log, db, date):
-        self.__date = date
-        super().__init__(log, db)
-
-    def keypress(self, size, key):
-        if key.startswith('meta'):
-            c = key[-1]
-            if c.lower() in (DAY, WEEK, MONTH, YEAR, '='):
-                self.__change_date(c)
-                return
-        return super().keypress(size, key)
-
-    def __change_date(self, c):
-        if c == '=':
-            self.__date = dt.date.today()
-        else:
-            delta = (-1 if c == c.lower() else 1, c.lower())
-            self.save()
-            self.__date = add_duration(self.__date, delta)
-        self.rebuild()
-        return
-
-    def _build(self, session):
-        return self._build_date(session, self.__date)
-
-    @abstractmethod
-    def _build_date(self, s, date):
-        pass
 
 
 class Diary(DateSwitcher):
--- ch2/lib/widgets.py
+++ ch2/lib/widgets.py
@@ -1,19 +1,87 @@
 
-from urwid import Pile, MainLoop, Filler, Divider, Frame, Text
+import datetime as dt
+from abc import abstractmethod
 
-from ..lib.utils import PALETTE
-from ..uweird.tui.decorators import Border
-from ..uweird.tui.tabs import Root
+from urwid import ExitMainLoop
 
-
-class App(MainLoop):
-
-    def __init__(self, log, title, msgbar, body, tab_list, session):
-        self.root = Root(log, Border(Frame(Filler(body, valign='top'),
-                                           header=Pile([Text(title), Divider()]),
-                                           footer=Pile([Divider(), msgbar]))),
-                         tab_list, session=session)
-        self.root.discover()
-        super().__init__(self.root, palette=PALETTE)
+from .date import DAY, WEEK, MONTH, YEAR, add_duration
+from ..uweird.tui.tabs import TabNode
 
 
+class App(TabNode):
+    '''
+    An urwid mainlopp, database session and tabs.
+    '''
+
+    def __init__(self, log, db):
+        self._log = log
+        self.__db = db
+        self.__session = None
+        super().__init__(log, *self._build(self.__new_session()))
+
+    def __new_session(self):
+        self.save()
+        self.__session = self.__db.session()
+        return self.__session
+
+    def keypress(self, size, key):
+        if key == 'meta q':
+            self.save()
+            raise ExitMainLoop()
+        elif key == 'meta x':
+            raise ExitMainLoop()
+        elif key == 'meta s':
+            self.save()
+        else:
+            return super().keypress(size, key)
+
+    def save(self):
+        if self.__session:
+            self._log.debug('Flushing and committing')
+            self.__session.flush()
+            self.__session.commit()
+            self.__session = None
+
+    @abstractmethod
+    def _build(self, session):
+        pass
+
+    def rebuild(self):
+        widget, tabs = self._build(self.__new_session())
+        self._w = widget
+        self.replace(tabs)
+
+
+class DateSwitcher(App):
+    '''
+    Extend App with shortcuts for changing date and rebuilding.
+    '''
+
+    def __init__(self, log, db, date):
+        self.__date = date
+        super().__init__(log, db)
+
+    def keypress(self, size, key):
+        if key.startswith('meta'):
+            c = key[-1]
+            if c.lower() in (DAY, WEEK, MONTH, YEAR, '='):
+                self.__change_date(c)
+                return
+        return super().keypress(size, key)
+
+    def __change_date(self, c):
+        if c == '=':
+            self.__date = dt.date.today()
+        else:
+            delta = (-1 if c == c.lower() else 1, c.lower())
+            self.save()
+            self.__date = add_duration(self.__date, delta)
+        self.rebuild()
+        return
+
+    def _build(self, session):
+        return self._build_date(session, self.__date)
+
+    @abstractmethod
+    def _build_date(self, s, date):
+        pass
