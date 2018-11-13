c756d86e4a78145f2c19c314caa2d460da982a3f
andrew cooke <andrew@acooke.org>
2018-11-13 10:47:25 -0300

    fixed up examples

--- ch2/data/database.py
+++ ch2/data/database.py
@@ -2,7 +2,6 @@
 from collections import defaultdict
 
 from pandas import DataFrame
-from pygeotile.point import Point
 from sqlalchemy.sql.functions import count
 
 from ..squeal.database import connect
@@ -10,9 +9,6 @@ from ..squeal.tables.activity import ActivityGroup, ActivityJournal
 from ..squeal.tables.monitor import MonitorJournal
 from ..squeal.tables.source import Interval, Source
 from ..squeal.tables.statistic import StatisticName, StatisticJournal, StatisticMeasure
-
-# todo
-MonitorSteps = None
 
 
 def extract(data, instance, *attributes):
@@ -34,7 +30,7 @@ class Data:
         self._log = log
         self._session = db.session()
 
-    def activities(self):
+    def activity_groups(self):
         data, ids = defaultdict(list), []
         for activity_group in self._session.query(ActivityGroup).order_by(ActivityGroup.name):
             ids.append(activity_group.id)
@@ -50,31 +46,14 @@ class Data:
             q = q.filter(ActivityJournal.finish >= start)
         if finish:
             q = q.filter(ActivityJournal.start <= finish)
+        self._log.debug(q)
         for journal in q.order_by(ActivityJournal.start).all():
-            times.append(journal.time)
-            extract(data, journal, 'name', 'fit_file', 'finish')
+            times.append(journal.start)
+            extract(data, journal, 'id', 'name', 'fit_file', 'finish')
+        data['source_id'] = data['id']; del data['id']
         return DataFrame(data, index=times)
 
-    def activity_waypoints(self, activity, time):
-        journal = self._session.query(ActivityJournal).join(ActivityGroup). \
-            filter(ActivityGroup.name == activity, ActivityJournal.time == time).one()
-        frames = []
-        for timespan in journal.timespans:
-            data, times = defaultdict(list), []
-            for waypoint in timespan.waypoints:
-                times.append(waypoint.time)
-                extract(data, waypoint, 'latitude', 'longitude', 'heart_rate', 'distance', 'speed')
-                if waypoint.latitude and waypoint.longitude:
-                    p = Point.from_latitude_longitude(waypoint.latitude, waypoint.longitude)
-                    x, y = p.meters
-                else:
-                    x, y = None, None
-                data['x'].append(x)
-                data['y'].append(y)
-            frames.append(DataFrame(data, index=times))
-        return frames
-
-    def statistics(self, *statistics):
+    def statistic_names(self, *statistics):
         statistic_names, statistic_ids = self._collect_statistics(statistics)
         data = defaultdict(list)
         for statistic in self._session.query(StatisticName). \
@@ -96,25 +75,29 @@ class Data:
         return statistic_names, statistic_ids
 
     def _build_statistic_journal_query(self, statistic_ids, q,
-            start, finish, owner, constraint):
+            start, finish, owner, constraint, source_id):
         q = q.filter(StatisticName.id.in_(statistic_ids))
         if start:
-            q = q.filter(Source.time >= start)
+            q = q.filter(StatisticJournal.time >= start)
         if finish:
-            q = q.filter(Source.time <= finish)
+            q = q.filter(StatisticJournal.time <= finish)
         if owner:
             q = q.filter(StatisticName.owner == owner)
         if constraint:
             q = q.filter(StatisticName.constraint == constraint)
+        if source_id:
+            q = q.filter(StatisticJournal.source_id == int(source_id))
         return q
 
-    def statistic_journals(self, *statistics, start=None, finish=None, owner=None, constraint=None, schedule=None):
+    def statistic_journals(self, *statistics,
+                           start=None, finish=None, owner=None, constraint=None, schedule=None, source_id=None):
         statistic_names, statistic_ids = self._collect_statistics(statistics)
         q = self._build_statistic_journal_query(
             statistic_ids, self._session.query(StatisticJournal).join(StatisticName, Source),
-            start, finish, owner, constraint)
+            start, finish, owner, constraint, source_id)
         if schedule:
             q = q.join(Interval).filter(Interval.schedule == schedule)
+        self._log.debug(q)
         raw_data = defaultdict(dict)
         for journal in q.all():  # todo - eager load
             raw_data[journal.time][journal.statistic_name.name] = journal.value
@@ -129,20 +112,22 @@ class Data:
                     data[statistic].append(None)
         return DataFrame(data, index=times)
 
-    def statistic_quartiles(self, *statistics, schedule='m', start=None, finish=None, owner=None, constraint=None):
+    def statistic_quartiles(self, *statistics, schedule='m',
+                            start=None, finish=None, owner=None, constraint=None, source_id=None):
         statistic_names, statistic_ids = self._collect_statistics(statistics)
         q = self._session.query(StatisticMeasure). \
             join(StatisticJournal, StatisticMeasure.statistic_journal_id == StatisticJournal.id). \
             join(StatisticName, StatisticJournal.statistic_name_id == StatisticName.id). \
             join(Source, StatisticJournal.source_id == Source.id)
-        q = self._build_statistic_journal_query(statistic_ids, q, start, finish, owner, constraint)
+        q = self._build_statistic_journal_query(statistic_ids, q, start, finish, owner, constraint, source_id)
         if schedule:
             q = q.join((Interval, StatisticMeasure.source_id == Interval.id)). \
                 filter(Interval.schedule == schedule)
         q = q.filter(StatisticMeasure.quartile != None)
+        self._log.debug(q)
         raw_data = defaultdict(lambda: defaultdict(lambda: [0] * 5))
         for measure in q.all():
-            raw_data[measure.source.time][measure.statistic_journal.statistic_name.name][measure.quartile] = \
+            raw_data[measure.source.start][measure.statistic_journal.statistic_name.name][measure.quartile] = \
                 measure.statistic_journal.value
         data, times = defaultdict(list), []
         for time in sorted(raw_data.keys()):
@@ -157,27 +142,10 @@ class Data:
 
     def monitor_journals(self):
         data, times = defaultdict(list), []
-        for journal in self._session.query(MonitorJournal).order_by(MonitorJournal.time).all():
-            times.append(journal.time)
-            extract(data, journal, 'fit_file', 'finish')
-        return DataFrame(data, index=times)
-
-    def monitor_steps(self, time):
-        data, times = defaultdict(list), []
-        for steps in self._session.query(MonitorSteps).join(MonitorJournal). \
-                filter(MonitorJournal.time == time). \
-                order_by(MonitorSteps.time).all():
-            times.append(steps.time)
-            extract(data, steps, 'value', 'activity')
-        return DataFrame(data, index=times)
-
-    def monitor_heart_rate(self, time):
-        data, times = defaultdict(list), []
-        for heart_rate in self._session.query(MonitorHeartRate).join(MonitorJournal). \
-                filter(MonitorJournal.time == time). \
-                order_by(MonitorHeartRate.time).all():
-            times.append(heart_rate.time)
-            extract(data, heart_rate, 'value')
+        for journal in self._session.query(MonitorJournal).order_by(MonitorJournal.start).all():
+            times.append(journal.start)
+            extract(data, journal, 'id', 'fit_file', 'finish')
+        data['source_id'] = data['id']; del data['id']
         return DataFrame(data, index=times)
 
 
