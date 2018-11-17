e0e0aa80f9196f4e1e7d8dc015daa5aee6857037
andrew cooke <andrew@acooke.org>
2018-11-17 14:02:17 -0300

    version 0.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(name='choochoo',
                  packages=setuptools.find_packages(),
-                 version='v0.5.0',
+                 version='0.5.1',
                  author='andrew cooke',
                  author_email='andrew@acooke.org',
                  description='A Programmable Training Diary',
