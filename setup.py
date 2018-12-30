f414f5da520cc78fc5d6d686f652709be2528d00
andrew cooke <andrew@acooke.org>
2018-12-30 13:48:39 -0300

    version 0.10.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(name='choochoo',
                  packages=setuptools.find_packages(),
-                 version='0.10.3',
+                 version='0.10.4',
                  author='andrew cooke',
                  author_email='andrew@acooke.org',
                  description='Data Science for Training',
