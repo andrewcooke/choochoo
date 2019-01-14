c0012b2e019237b28c6f67f093e049dd581cf925
andrew cooke <andrew@acooke.org>
2019-01-13 22:24:25 -0300

    optimize slightly

--- ch2/sortem/__init__.py
+++ ch2/sortem/__init__.py
@@ -33,9 +33,11 @@ class ElevationOracle:
 
     def elevation(self, lat, lon):
         if self._dir:
-            h = cached_file_reader(join(self._dir, self.file_name(lat, lon)))
-            x = (lon - floor(lon)) * (SAMPLES - 1)
-            y = (lat - floor(lat)) * (SAMPLES - 1)  # -1 because weird inclusive-at-each-side tiling
+            flat, flon = floor(lat), floor(lon)
+            # construct the path in the reader so it's skipped if we hit the cache
+            h = cached_file_reader(self._dir, flat, flon)
+            x = (lon - flon) * (SAMPLES - 1)
+            y = (lat - flat) * (SAMPLES - 1)  # -1 because weird inclusive-at-each-side tiling
             i, j = int(x), int(y)
             # bilinear
             # it's ok to use +1 blindly here because we're never on the top/right cells or we'd be in
@@ -48,16 +50,14 @@ class ElevationOracle:
         else:
             return None
 
+
+@lru_cache(4)  # 4 means our tests are quick...
+def cached_file_reader(dir, flat, flon):
     # https://wiki.openstreetmap.org/wiki/SRTM
     # The official 3-arc-second and 1-arc-second data for versions 2.1 and 3.0 are divided into 1°×1° data tiles.
     # The tiles are distributed as zip files containing HGT files labeled with the coordinate of the southwest cell.
     # For example, the file N20E100.hgt contains data from 20°N to 21°N and from 100°E to 101°E inclusive.
-    def file_name(self, lat, lon):
-        return '%s%02d%s%03d.hgt' % \
-               ('S' if lat < 0 else 'N', abs(floor(lat)), 'W' if lon < 0 else 'E', abs(floor(lon)))
-
-
-@lru_cache(4)  # 4 means our tests are quick...
-def cached_file_reader(path):
-    with open(path, 'rb') as data:
+    file = '%s%02d%s%03d.hgt' % \
+           ('S' if flat < 0 else 'N', abs(flat), 'W' if flon < 0 else 'E', abs(flon))
+    with open(join(dir, file), 'rb') as data:
         return np.flip(np.fromfile(data, np.dtype('>i2'), SAMPLES * SAMPLES).reshape((SAMPLES, SAMPLES)), 0)
