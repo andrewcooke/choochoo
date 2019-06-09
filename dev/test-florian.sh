1a9532533523cc8a9d1e1f308de68b98e4c47eb0
andrew cooke <andrew@acooke.org>
2019-06-09 12:22:23 -0400

    kill old processes

--- dev/test-florian.sh
+++ dev/test-florian.sh
@@ -12,6 +12,7 @@ source env/bin/activate
 ch2 --dev -v5 package-fit-profile /home/andrew/project/ch2/choochoo/data/sdk/Profile.xlsx
 ch2 --dev -v5 -f `pwd`/ch2.sql config default
 ch2 --dev -v5 -f `pwd`/ch2.sql activities /home/andrew/project/ch2/choochoo/data/test/source/private/florian.fit /home/andrew/project/ch2/choochoo/data/test/source/other/920* -Kn_cpu=1
+pkill -f jupyter
 ch2 --dev -v5 -f `pwd`/ch2.sql jupyter show calendar
 ch2 --dev -v5 -f `pwd`/ch2.sql jupyter show activity_details 2019-05-16 Bike
 echo
