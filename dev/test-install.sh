95170a00f8e182db1efb3f97692869575aa3532e
andrew cooke <andrew@acooke.org>
2020-01-04 18:20:15 -0300

    tweak test

--- dev/test-install.sh
+++ dev/test-install.sh
@@ -2,6 +2,10 @@
 
 # use 'official' version (not CH2_VERSION)
 VERSION=`grep 'version=' setup.py | sed -e "s/.*version='\([0-9]\+\.[0-9]\+\.[0-9]\+\)'.*/\1/"`
+echo "VERSION=$VERSION"
+
+# enable ** globbing
+shopt -s globstar
 
 cd /tmp
 rm -fr ch2
@@ -12,5 +16,5 @@ source env/bin/activate
 pip install --upgrade pip
 pip install choochoo==$VERSION || exit 1
 ch2 --dev -f `pwd`/ch2.sql config default
-ch2 --dev -f `pwd`/ch2.sql activities --fast ~/archive/fit/bike/*.fit
+ch2 --dev -f `pwd`/ch2.sql activities --fast ~/archive/fit/bike/**/*.fit
 ch2 --dev -f `pwd`/ch2.sql monitor ~/archive/fit/monitor/*.fit
