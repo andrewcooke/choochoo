970cbb05d50d2be4886c5178e5e58cbdce1fe1d3
andrew cooke <andrew@acooke.org>
2020-02-01 21:50:05 -0300

    initial expanded tree

--- js/src/components/pages/diary/fmtDay.jsx
+++ js/src/components/pages/diary/fmtDay.jsx
@@ -3,8 +3,38 @@ import {TreeView, TreeItem} from '@material-ui/lab';
 
 
 export default function fmtDay(json) {
-    return <p>Loaded day data</p>;
+    const ids = addIds(json);
+    return (<TreeView defaultExpanded={ids}>{fmtList(json)}</TreeView>);
 }
 
 
-const foo = <TreeView><TreeItem/></TreeView>
+function fmtList(json) {
+    if (Array.isArray(json)) {
+        const head = json[0];
+        return <TreeItem key={json.id} nodeId={json.id} label={head.value}>{json.slice(1).map(fmtList)}</TreeItem>;
+    } else {
+        return <p key={json.id}>{json.label}={json.value}</p>;
+    }
+}
+
+
+function addIds(json) {
+
+    function add(base) {
+        return (json, index) => {
+            const id = (base === undefined) ? `${index}` : `${base},${index}`;
+            json.id = id;
+            if (Array.isArray(json)) json.map(add(id));
+        }
+    }
+    add()(json, 0);
+
+    let ids = [];
+    function list(json) {
+        ids.push(json.id);
+        if (Array.isArray(json)) json.map(list);
+    }
+    list(json);
+
+    return ids
+}
