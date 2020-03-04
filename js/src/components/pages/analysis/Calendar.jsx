933c2c710d758c067e4a9512bc62129da2aa0e49
andrew cooke <andrew@acooke.org>
2020-03-04 10:14:53 -0300

    example card for analysis - calendar

--- js/src/components/pages/analysis/Calendar.jsx
+++ js/src/components/pages/analysis/Calendar.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import {ColumnCard} from "../../elements";
+import {ColumnCard, LinkButton, Text} from "../../elements";
+import {Grid} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles(theme => ({
+    center: {
+        textAlign: 'center',
+    },
+    left: {
+        textAlign: 'left',
+    },
+    right: {
+        textAlign: 'right',
+    },
+}));
 
 
 export default function Calendar(props) {
+    const classes = useStyles();
     return (<ColumnCard header='Calendar'>
-        <p>stuff</p>
+        <Grid item xs={12}><Text>
+            <p>Various representations of all activities, across all dates, showing distance, time, SHRIMP, etc.</p>
+        </Text></Grid>
+        <Grid item xs={12} className={classes.right}><LinkButton href='jupyter/calendar'>Display</LinkButton></Grid>
     </ColumnCard>);
 }
