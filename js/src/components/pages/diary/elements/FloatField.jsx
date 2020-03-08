a5578eb55810e3cc7bf2564c7027e788c7898967
andrew cooke <andrew@acooke.org>
2020-03-08 12:08:37 -0300

    allow decimal in floats

--- js/src/components/pages/diary/elements/FloatField.jsx
+++ js/src/components/pages/diary/elements/FloatField.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import mkEditableField from "./mkEditableField";
 
 
-const FloatField = mkEditableField({rx: /^\d*$/, xs: 4});
+const FloatField = mkEditableField({rx: /^(\d+(.\d*)?)|(\d*.\d+)$/, xs: 4});
 export default FloatField;
