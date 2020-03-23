64db2488b49b9a3f5277fd5bcc241677d230c8e7
andrew cooke <andrew@acooke.org>
2020-03-22 21:39:58 -0300

    more change work

--- js/src/components/pages/kit/Change.jsx
+++ js/src/components/pages/kit/Change.jsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import {ColumnCard, ColumnList, Layout, LinkButton, Loading, MainMenu, Text} from "../../elements";
-import {Grid, Typography} from "@material-ui/core";
+import {Break, ColumnCard, ColumnList, Layout, LinkButton, Loading, MainMenu, Text} from "../../elements";
+import {Grid, Typography, TextField, Box} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import {Autocomplete} from "@material-ui/lab";
 
 
 const useStyles = makeStyles(theme => ({
@@ -21,20 +22,11 @@ const useStyles = makeStyles(theme => ({
 
 
 function Button(props) {
-    const {href, label, xs=12} = props;
+    const {href, label, xs=12, disabled=false} = props;
     const classes = useStyles();
     return (<Grid item xs={xs} className={classes.right}>
-        <LinkButton href={href}>{label}</LinkButton>
+        <LinkButton href={href} disabled={disabled}>{label}</LinkButton>
     </Grid>);
-}
-
-
-function Added(props) {
-    const {added} = props;
-    return (<>
-        <Grid item xs={2}><Text>Added</Text></Grid>
-        <Grid item xs={2}><Text>{added}</Text></Grid>
-    </>);
 }
 
 
@@ -43,23 +35,59 @@ function ModelShow(props) {
     const classes = useStyles();
     return (<>
         <Grid item xs={12} className={classes.h3}>
-            <Typography variant='h3'>{model.name} / {component.name}</Typography>
+            <Typography variant='h3'>{component.name} / {model.added}</Typography>
         </Grid>
-        <Added added={model.added}/>
-        <Grid xs={5}>Autocomplete</Grid>
+        <Grid item xs={9}>
+            <Autocomplete options={component.models.map(model => model.name)} freeSolo
+                          defaultValue={model.name}
+                          renderInput={params => <TextField {...params} label=''/>}/>
+        </Grid>
         <Button xs={3} label='Replace'/>
     </>);
 }
 
 
+function AddComponent(props) {
+
+    const {item, allComponents} = props;
+    const [component, setComponent] = useState('');
+    const [model, setModel] = useState('');
+    const existing = item.components.map(component => component.name);
+    const components = Object.keys(allComponents).filter(component => ! existing.includes(component));
+    const models = components.includes(component) ? allComponents[component].map(model => model.name) : [];
+    const disabled = component === '' || model === '';
+
+    return (<>
+        <Grid item xs={12}>
+            <Box mt={3}><Text>To add a completely new component and model:</Text></Box>
+        </Grid>
+        <Grid item xs={9}>
+            <Autocomplete options={components} label='Component' freeSolo value={component}
+                          onInputChange={(event, value) => setComponent(value)}
+                          renderInput={params => <TextField {...params} label=''/>}/>
+        </Grid><Break/>
+        <Grid item xs={9}>
+            <Autocomplete options={models} label='Model' freeSolo value={model}
+                          onInputChange={(event, value) => setModel(value)}
+                          renderInput={params => <TextField {...params} label=''/>}/>
+        </Grid>
+        <Button xs={3} label='Add' disabled={disabled}/>
+    </>);
+}
+
+
 function ItemShow(props) {
-    const {item, group} = props;
-    return (<ColumnCard header={`${item.name} / ${group.name}`}>
-        <Added added={item.added}/>
-        <Button xs={8} label='Retire'/>
+    const {item, group, allComponents} = props;
+    let model_dbs = item.models.map(model => model.db);
+    return (<ColumnCard>
+        <Grid item xs={9}>
+            <Typography variant='h2'>{item.name} / {group.name} / {item.added}</Typography>
+        </Grid>
+        <Button xs={3} label='Retire'/>
         {item.components.map(
-            component => component.models.map(
+            component => component.models.filter(model => model_dbs.includes(model.db)).map(
                 model => <ModelShow model={model} component={component} key={model.db}/>)).flat()}
+        <AddComponent item={item} allComponents={allComponents}/>
     </ColumnCard>);
 }
 
@@ -71,10 +99,13 @@ function Columns(props) {
     if (groups === null) {
         return <Loading/>;  // undefined initial data
     } else {
+        const allComponents = {};
+        groups.forEach(group => group.items.forEach(item => item.components.forEach(
+            component => allComponents[component.name] = component.models)))
         return (<ColumnList>
             {groups.map(
                 group => group.items.map(
-                    item => <ItemShow item={item} group={group} key={item.db}/>)).flat()}
+                    item => <ItemShow item={item} group={group} key={item.db} allComponents={allComponents}/>)).flat()}
         </ColumnList>);
     }
 }
