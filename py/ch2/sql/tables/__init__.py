03eb6e3e3a84af428adb3dfe684e9d86a66bb45c
andrew <andrew@acooke.org>
2020-10-13 21:09:51 -0300

    drop segments (lots of stuff broken)

--- py/ch2/sql/tables/__init__.py
+++ py/ch2/sql/tables/__init__.py
@@ -9,7 +9,6 @@ from .kit import KitGroup, KitItem, KitComponent, KitModel
 from .monitor import MonitorJournal
 from .nearby import ActivitySimilarity, ActivityNearby
 from .pipeline import Pipeline, PipelineType
-from .segment import Segment, SegmentJournal
 from .sector import SectorGroup, Sector, SectorClimb
 from .source import Source, Interval, NoStatistics, Composite, CompositeComponent
 from .statistic import StatisticName, StatisticJournalFloat, StatisticJournalText, StatisticJournalInteger, \
