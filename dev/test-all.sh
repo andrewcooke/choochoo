3631f772aaa19ac450bb9e2bf721ba1211904f81
andrew <andrew@acooke.org>
2021-04-09 17:43:31 -0400

    start up db in tests

--- dev/test-all.sh
+++ dev/test-all.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+dkr/run-pg-transient.sh &
+PID=$!
+
+function cleanup {
+    kill $PID
+}
+
+trap cleanup EXIT
+echo "waiting for database startup"
+sleep 5
 source py/env/bin/activate
+ch2 db add user
+ch2 db add database
 PYTHONPATH=py python -m unittest py/tests/*.py
