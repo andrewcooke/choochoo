ecedd05f13c59b80ae779bb80f797ba7b0d557f2
andrew <andrew@acooke.org>
2021-04-28 10:27:09 -0400

    simplified test

--- py/tests/test_search.py
+++ py/tests/test_search.py
@@ -89,27 +89,14 @@ class SQLAlchemyTest2(LogTestCase):
 
         AND, OR, NULL = 'and', 'or', 'null'
         UNDEF = object()
-        WGS84_SRID = 4326
 
         class SourceType(IntEnum):
             SOURCE = 0
-            INTERVAL = 1
             ACTIVITY = 2
-            DIARY_TOPIC = 3
-            CONSTANT = 4
-            MONITOR = 5
-            SEGMENT = 6
-            COMPOSITE = 7
-            ITEM = 8
-            MODEL = 9
             ACTIVITY_TOPIC = 10
-            SECTOR = 11
 
         class StatisticJournalType(IntEnum):
             STATISTIC = 0
-            INTEGER = 1
-            FLOAT = 2
-            TEXT = 3
             TIMESTAMP = 4
 
         class UTC(TypeDecorator):
@@ -128,8 +115,6 @@ class SQLAlchemyTest2(LogTestCase):
             __tablename__ = 'source'
             id = Column(Integer, primary_key=True)
             type = Column(Integer, nullable=False, index=True)
-            # activity_group_id = Column(Integer, ForeignKey('activity_group.id', ondelete='cascade'), nullable=True)
-            # activity_group = relationship('ActivityGroup')
             __mapper_args__ = {
                 'polymorphic_identity': SourceType.SOURCE,
                 'polymorphic_on': type
@@ -145,12 +130,7 @@ class SQLAlchemyTest2(LogTestCase):
             file_hash = relationship('FileHash', backref=backref('activity_journal', uselist=False))
             start = Column(UTC, nullable=False, index=True, unique=True)
             finish = Column(UTC, nullable=False)
-            # centre = Column(Geography('Point', srid=WGS84_SRID))
             utm_srid = Column(Integer)
-            # route_a = Column(Geography('LineStringM', srid=WGS84_SRID))  # delta azimuth
-            # route_d = Column(Geography('LineStringM', srid=WGS84_SRID))  # distance
-            # route_et = Column(Geography('LineStringZM', srid=WGS84_SRID))  # time
-            # route_edt = Column(Geography('LineStringZM', srid=WGS84_SRID))  # elevation, distance / m * 1e7 + elapsed time
             __mapper_args__ = {
                 'polymorphic_identity': SourceType.ACTIVITY
             }
@@ -177,18 +157,6 @@ class SQLAlchemyTest2(LogTestCase):
             owner = Column(Text, nullable=False, index=True)
             statistic_journal_type = Column(Integer, nullable=False)
             UniqueConstraint(name, owner)
-            @classmethod
-            def parse(cls, qname, default_owner=None, default_activity_group=None):
-                if ':' in qname:
-                    left, group = qname.rsplit(':', 1)
-                else:
-                    left, group = qname, default_activity_group
-                if '.' in left:
-                    owner, name = left.rsplit('.', 1)
-                else:
-                    owner, name = default_owner, left
-                log.debug(f'Parsed {qname} as {owner}.{name}:{group}')
-                return owner, name, group
 
         class StatisticJournal(Base):
             __tablename__ = 'statistic_journal'
@@ -216,106 +184,23 @@ class SQLAlchemyTest2(LogTestCase):
                 'polymorphic_identity': StatisticJournalType.TIMESTAMP
             }
 
-        STATISTIC_JOURNAL_CLASSES = {
-            # StatisticJournalType.INTEGER: StatisticJournalInteger,
-            # StatisticJournalType.FLOAT: StatisticJournalFloat,
-            # StatisticJournalType.TEXT: StatisticJournalText,
-            StatisticJournalType.TIMESTAMP: StatisticJournalTimestamp
-        }
         Base.metadata.create_all(engine)
 
-        def get_op_attr(op, value):
-            attrs = {'=': '__eq__', '!=': '__ne__', '>': '__gt__', '>=': '__ge__', '<': '__lt__', '<=': '__le__'}
-            if isinstance(value, str): attrs.update({'=': 'ilike', '!=': 'nlike'})
-            return attrs[op]
-
-        def get_source_ids(s, owner, name, op, value, group, type):
-            op_attr = get_op_attr(op, value)
-            statistic_journal = STATISTIC_JOURNAL_CLASSES[type]
+        def build_source_query(s, value):
             q = s.query(Source.id). \
-                join(statistic_journal). \
+                join(StatisticJournalTimestamp). \
                 join(StatisticName). \
-                filter(StatisticName.name.like(name))
-            if owner:
-                q = q.filter(StatisticName.owner.like(owner))
-            if group is not UNDEF:
-                raise Exception('not used in test')
-            #     if group:
-            #         q = q.join(ActivityGroup).filter(ActivityGroup.name.ilike(group))
-            #     else:
-            #         q = q.filter(Source.activity_group_id == None)
-            if op_attr == 'nlike':
-                q = q.filter(not_(statistic_journal.value.ilike(value)))
-            else:
-                q = q.filter(getattr(statistic_journal.value, op_attr)(value))
-            return q
-
-        def build_comparisons(s, ast, with_conversion):
-            qname, op, value = ast
-            owner, name, group = StatisticName.parse(qname, default_activity_group=UNDEF)
-            if value is None:
-                if op == '=':
-                    raise Exception('not used in test')
-                    # return get_source_ids_for_null(s, owner, name, group, with_conversion), True
-                else:
-                    return union(*[get_source_ids(s, owner, name, op, value, group, type)
-                                   for type in StatisticJournalType
-                                   if type != StatisticJournalType.STATISTIC]).subquery().select(), False
-            elif isinstance(value, str):
-                raise Exception('not used in test')
-                # return get_source_ids(s, owner, name, op, value, group, StatisticJournalType.TEXT), False
-            elif isinstance(value, dt.datetime):
-                return get_source_ids(s, owner, name, op, value, group, StatisticJournalType.TIMESTAMP), False
-            else:
-                raise Exception('not used in test')
-                # qint = get_source_ids(s, owner, name, op, value, group, StatisticJournalType.INTEGER)
-                # qfloat = get_source_ids(s, owner, name, op, value, group, StatisticJournalType.FLOAT)
-                # return union(qint, qfloat).subquery().select(), False
-
-        def build_constraint(s, ast, attrs, with_conversion):
-            qname, op, value = ast
-            if qname in attrs:
-                raise Exception('not used in test')
-                # return build_property(s, ast), False
-            else:
-                return build_comparisons(s, ast, with_conversion)
-
-        def build_constraints(s, ast, attrs, conversion=None):
-            l, op, r = ast
-            if op in (AND, OR):
-                raise Exception('not used in test')
-                # lcte = build_constraints(s, l, attrs, conversion=conversion)
-                # rcte = build_constraints(s, r, attrs, conversion=conversion)
-                # return build_join(op, lcte, rcte)
-            else:
-                constraint, null = build_constraint(s, ast, attrs, bool(conversion))
-                if conversion: constraint = conversion(s, constraint, null)
-                return constraint
-
-        def build_source_query(s, ast, attrs, conversion=None):
-            constraints = build_constraints(s, ast, attrs, conversion=conversion).cte()
-            return s.query(Source).filter(Source.id.in_(constraints))
-
-        def constrained_sources(s, ast, conversion=None):
-            attrs = set()
-            q = build_source_query(s, ast, attrs, conversion=conversion)
-            return q.all()
-
-        def activity_conversion(s, source_ids, null):
+                filter(StatisticName.name.like('start')). \
+                filter(StatisticJournalTimestamp.value > value)
             q_direct = s.query(ActivityJournal.id). \
-                filter(ActivityJournal.id.in_(source_ids.subquery()))
+                filter(ActivityJournal.id.in_(q.subquery()))
             q_via_topic = s.query(ActivityJournal.id). \
                 join(FileHash). \
                 join(ActivityTopicJournal). \
-                filter(ActivityTopicJournal.id.in_(source_ids.subquery()))
-            q = union(q_direct, q_via_topic).subquery().select()
-            if null:
-                return s.query(ActivityJournal.id).filter(not_(ActivityJournal.id.in_(q)))
-            else:
-                return q
+                filter(ActivityTopicJournal.id.in_(q.subquery()))
+            constraints = union(q_direct, q_via_topic).subquery().select()
+            return s.query(Source).filter(Source.id.in_(constraints))
 
         with Session() as s:
-            constrained_sources(s, ('start', '>', dt.datetime(2020, 1, 1, 3, 0, tzinfo=pytz.UTC)),
-                                conversion=activity_conversion)
-            constrained_sources(s, ('start', '>', dt.datetime(2021, 1, 1, 3, 0, tzinfo=pytz.UTC)),
-                                conversion=activity_conversion)
+            build_source_query(s, dt.datetime(2020, 1, 1, 3, 0, tzinfo=pytz.UTC)).all()
+            build_source_query(s, dt.datetime(2021, 1, 1, 3, 0, tzinfo=pytz.UTC)).all()
